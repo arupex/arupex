97e41413c409267ed5d4552e482d61d6dcf53951
Daniel Irwin <daniel.irwin@enernoc.com>
2017-07-06 16:15:07 -0400

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# arupex
+#arupex
+Arupex is a Serverless/Server framework for NodeJS
+It allows you to build a serverless application for your needs, but also allows you to wrap that with a http server if need be
+
+![Logo](http://arupex.com/img/logo.png)
+
+[![npm version](https://badge.fury.io/js/arupex.svg)](https://badge.fury.io/js/arupex)
+[![dependencies](https://david-dm.org/arupex/areupex.svg)](http://github.com/arupex/arupex)
+![Build Status](https://api.travis-ci.org/arupex/arupex.svg?branch=master) 
+[![Donate](https://img.shields.io/badge/Donate-Arupex-green.svg)](https://pledgie.com/campaigns/31873)
+![lifetimeDownloadCount](https://img.shields.io/npm/dt/arupex.svg?maxAge=2592000)
+
+##Goals
+- Making Testing easier on Developers
+ - Making Mocking a thing of the past
+ - Simplifying Workflows
+ - Making Dependency Injection simple and elegant
+
+Check out our [Documentation](http://arupex.com)
--- interceptors/lambdInterceptor.js
+++ interceptors/lambdInterceptor.js
@@ -4,64 +4,106 @@
 module.exports = function(opts){
 
     let directoryLoader = require('../lib/requireDirectory');
-    let dir = opts.dir || process.cwd();
+    let injector        = require('../lib/injector');
+    let cookieParser    = require('../lib/cookieParser');
+    let logger          = require('../lib/logger');
+    let tracer          = require('../lib/trace');
+    let meter           = require('../lib/meter');
+    let pipeline        = require('../lib/pipeline');
+    let i18n            = require('../lib/i18n');
+    let docGenerator    = require('../lib/docGenerator');
+    let structured      = require('../lib/structured');
+    let clientBuilder   = require('../lib/clientBuilder');
 
-    let services = directoryLoader.requireDirSync(opts.serviceDir || `${dir}/Services`, opts.watch || opts.watchServicesDir);
+    let dir             = opts.dir || process.cwd();
 
-    let dataServices = directoryLoader.requireDirSync(opts.dataServiceDir || `${dir}/DataServices`, opts.watch || opts.watchDataServicesDir);
-
-    let policies = directoryLoader.requireDirSync(opts.policyDir || `${dir}/Policies`, opts.watch || opts.watchPoliciesDir);
-
-    let envs = directoryLoader.requireDirSync(opts.environmentsDir || `${dir}/Environments`, opts.watch || opts.watchEnvironmentsDir);
+    let services        = directoryLoader.requireDirSync(opts.serviceDir         || `${dir}/Services`,     opts.watch || opts.watchServicesDir);
+    let dataServices    = directoryLoader.requireDirSync(opts.dataServiceDir     || `${dir}/DataServices`, opts.watch || opts.watchDataServicesDir);
+    let policies        = directoryLoader.requireDirSync(opts.policyDir          || `${dir}/Policies`,     opts.watch || opts.watchPoliciesDir);
+    let responses       = directoryLoader.requireDirSync(opts.responsesDir       || `${dir}/Responses`,    opts.watch || opts.watchResponsesDir);
+    let lambdaFunctions = directoryLoader.requireDirSync(opts.lambdaFunctionsDor || `${dir}/Functions`,    opts.watch || opts.watchLambdaFunctionsDir);
+    let envs            = directoryLoader.requireDirSync(opts.environmentsDir    || `${dir}/Environments`, opts.watch || opts.watchEnvironmentsDir);
 
     let activeEnvironment = envs[process.env.ENVIRONMENT || 'dev'];
     if(!activeEnvironment){
         process.stderr.write('error no active environment to set the active environment set the ENVIRONMENT variable\n');
     }
 
-    let responses = directoryLoader.requireDirSync(opts.responsesDir || `${dir}/Responses`, opts.watch || opts.watchResponsesDir);
-
-    let lambdaFunctions = directoryLoader.requireDirSync(opts.lambdaFunctionsDor || `${dir}/Functions`, opts.watch || opts.watchLambdaFunctionsDir);
-
-    let clientBuilder = require('../lib/clientBuilder');
-
-    let injectableDataServices = {};
-    if(dataServices){
-
-        injectableDataServices = Object.keys(dataServices).reduce((acc, v) => {
-            if(Array.isArray(dataServices[v])) {
-                if(!activeEnvironment[v]){
-                    process.emit('error', 'environment did not include config for ' + v);
-                }
-                acc[v] = clientBuilder(dataServices[v]).init(activeEnvironment[v]);
-            }
-            else {
-                acc[v] = dataServices[v];//not a client
-            }
-            return acc;
-        }, {});
-
-    }
-
-    let injector = require('../lib/injector');
-    let cookieParser = require('../lib/cookieParser');
-    let logger = require('../lib/logger');
-    let tracer = require('../lib/trace');
-    let meter = require('../lib/meter');
-    let pipeline = require('../lib/pipeline');
-    let i18n = require('../lib/i18n');
-    let docGenerator = require('../lib/docGenerator');
-
-    let structured = require('../lib/structured');
-
     let mockDataProperty = opts.mockDataProperty || 'mockData';
 
-    let routes = require(opts.routesFile || `${dir}/routes`);
+    let injectableDataServices = {};
+
+    function calculateInjectableDataServices(nonInjectableServices) {
+        if (nonInjectableServices) {
+            return Object.keys(nonInjectableServices).reduce((acc, v) => {
+                if (Array.isArray(nonInjectableServices[v])) {
+                    if (!activeEnvironment[v]) {
+                        process.emit('error', 'environment did not include config for ' + v);
+                    }
+                    acc[v] = clientBuilder(nonInjectableServices[v]).init(activeEnvironment[v]);
+                }
+                else {
+                    acc[v] = nonInjectableServices[v];//not a client
+                }
+                return acc;
+            }, {});
+        }
+        return {};
+    };
+
+    injectableDataServices = calculateInjectableDataServices(dataServices);
 
     let policiesArray = Object.keys(policies).reduce((acc, v) => {acc.push(policies[v]); return acc;}, []);
 
-    return Object.keys(lambdaFunctions).reduce( (acc, lambdaName) => {
+    function initServices(useableDataServices, services) {
+        return Object.keys(services).reduce((acc, serviceName) => {
+            if (typeof services[serviceName] === 'function') {
+                acc[serviceName] = injector(useableDataServices, services[serviceName]);
+            }
+            else {
+                acc[serviceName] = services[serviceName];
+            }
+            return acc;
+        }, {});
+    }
 
+    function initMiddleware(middleware, injectableResponse, useableDataServices, instantiatedServices) {
+        return (middleware).reduce((acc, v) => {
+            acc.push(function (req, res, next) {
+                let injectables = Object.assign(Object.assign({
+                    req: req,
+                    res: injectableResponse,
+                    next: next,
+                    i18n: i18n,
+                    meter: meter,
+                    tracer: tracer,
+                    logger: logger
+                }, useableDataServices), instantiatedServices);
+
+                injector(injectables, v);
+            });
+            return acc;
+        }, []);
+    }
+
+
+    function initResponses(responses, callback) {
+        return Object.keys(responses).reduce((acc, v) => {
+            acc[v] = function (data) {//it is assumed that responses have 1 external parameter from the user (function)
+                injector({callback: callback, data: data}, responses[v]);
+            };
+            return acc;
+        }, {});
+    }
+
+
+    let metricTracer    = require('../lib/metricTracer')(function meterFinish(meter){
+console.log('meter', meter);
+    }, function traceFinish(type, traceName, value, other, traceRoute){
+console.log('trace', type, traceName, value, other, traceRoute);
+    });
+
+    return Object.keys(lambdaFunctions).reduce( (acc, lambdaName) => {
 
         let middleware = policiesArray.concat(lambdaFunctions[lambdaName]);
         let useableDataServices = injectableDataServices;
@@ -70,41 +112,24 @@ module.exports = function(opts){
 
             if(event[mockDataProperty] || context[mockDataProperty]){
                 useableDataServices = structured.toImplentation(event[mockDataProperty] || context[mockDataProperty]);
+                Object.keys(injectableDataServices).forEach((serviceName) => {
+                   if(typeof injectableDataServices[serviceName].overrideable !== 'undefined' && !injectableDataServices[serviceName].overrideable){//if false but not falsey
+                       useableDataServices[serviceName] = injectableDataServices[serviceName];
+                   }
+                });
             }
 
-            let instantiatedServices = Object.keys(services).reduce((acc, serviceName) => {
-              if(typeof services[serviceName] === 'function'){
-                  acc[serviceName] = injector(useableDataServices, services[serviceName]);
-              }
-              else {
-                  acc[serviceName] = services[serviceName];
-              }
-              return acc;
-            }, {});
+            useableDataServices = initServices({
+                event: event,
+                context: context,
+                meter: meter,
+                tracer: tracer,
+                logger: logger
+            }, useableDataServices);//allow dataservices to have event or context injected within
 
-            let injectableResponse = Object.keys(responses).reduce((acc, v) => {
-                acc[v] = function(data) {
-                    injector({callback: callback, data : data }, responses[v]);
-                };
-                return acc;
-            }, {});
-
-            let injectableMiddlware = (middleware).reduce((acc, v) => {
-                acc.push(function(req, res, next) {
-                    let injectables = Object.assign(Object.assign({
-                        req : req,
-                        res : injectableResponse,
-                        next : next,
-                        i18n : i18n,
-                        meter : meter,
-                        tracer : tracer,
-                        logger : logger
-                    }, useableDataServices), instantiatedServices);
-
-                    injector(injectables, v);
-                });
-                return acc;
-            }, []);
+            let instantiatedServices = initServices(useableDataServices, services);
+            let injectableResponse   = initResponses(responses, callback);
+            let injectableMiddlware  = initMiddleware(middleware, injectableResponse, useableDataServices, instantiatedServices);
 
             let lambdaPipeline = pipeline({
                 timeout : false
