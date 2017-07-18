edfce4bf162fc37f8a9a2842f736c79c36cf6142
Daniel Irwin <daniel.irwin@enernoc.com>
2017-07-17 22:13:28 -0400

    add filter to requireDirectory have multiDirLoader fallback to recusive dir read if tree cmd fails robust filter function in lambdas interceptor robust filter function in main arupex.js file

--- arupex.js
+++ arupex.js
@@ -4,7 +4,14 @@
 
 let loadDirectories = require(__dirname + '/lib/requireDirectory').requireDirSync;
 
+function ignoreBin(filename){
+    return filename.indexOf('bin') === -1 &&
+    filename.indexOf('demo') === -1  &&
+    filename.indexOf('tmp') === -1 &&
+    filename.indexOf('test') === -1;
+}
+
 module.exports = {
-    interceptors : loadDirectories(__dirname + '/interceptors'),
-    lib : loadDirectories(__dirname + '/lib')
+    interceptors : loadDirectories(__dirname + '/interceptors', ignoreBin),
+    lib : loadDirectories(__dirname + '/lib', ignoreBin)
 };
