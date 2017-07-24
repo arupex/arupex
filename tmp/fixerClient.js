fbc79f731de07251a0ab2f6992255193e97d9171
Daniel Irwin <daniel.irwin@enernoc.com>
2017-07-24 14:22:17 -0400

    fix logger in clientBuilder

--- tmp/fixerClient.js
+++ tmp/fixerClient.js
@@ -1,11 +1,23 @@
 module.exports = {
 init:function (restOpts, debug){
    let HyperRequest = require('hyper-request');
+   this.log = restOpts.logger || {
+       info : () => {  },
+       critical : () => {  }
+   };
+
    this.restClient = new HyperRequest(restOpts);
    this.interpolate = function (str) {
        return function interpolate(o) {
-      return str.replace(new RegExp('{{(.*)}}', 'g'), function (a, b) {
-          return typeof o[b] === 'string' || typeof o[b] === 'number' ? o[b] : a;
+      if(!o){
+          this.log.critical(`${str} could not be evaluated with falsey interpolation input`);
+          return str;
+      }
+      return str.replace(new RegExp('{{([^{}]+)}}', 'g'), function (a, b) {
+          if(!o[b]) {
+         this.log.critical(`${str} could not be evaluated with missing key ${b} during interpolation`);
+          }
+          return o[b];
       });
        };
    };
@@ -18,9 +30,7 @@ deleteAttachmentsWithId:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("attachments/{{id}}")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["delete"](url, opts, ok, fail);
    },
 deleteMeasuresWithMeasureIdByHard_delete:function (opts, ok, fail) {
@@ -28,9 +38,7 @@ deleteMeasuresWithMeasureIdByHard_delete:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures/{{measureId}}?hard_delete={{hard_delete}}")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["delete"](url, opts, ok, fail);
    },
 getAttachmentsWithId:function (opts, ok, fail) {
@@ -38,9 +46,7 @@ getAttachmentsWithId:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("attachments{{id}}")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["get"](url, opts, ok, fail);
    },
 getLatestByBase:function (opts, ok, fail) {
@@ -48,9 +54,7 @@ getLatestByBase:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("latest?base={{base}}")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["get"](url, opts, ok, fail);
    },
 getMeasuresByNode_id:function (opts, ok, fail) {
@@ -58,9 +62,7 @@ getMeasuresByNode_id:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures?node_id={{node_id}}")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["get"](url, opts, ok, fail);
    },
 getMeasuresWithMeasureIdActivity:function (opts, ok, fail) {
@@ -68,9 +70,7 @@ getMeasuresWithMeasureIdActivity:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures/{{measureId}}/activity")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["get"](url, opts, ok, fail);
    },
 getMeasuresWithMeasureIdAttachments:function (opts, ok, fail) {
@@ -78,9 +78,7 @@ getMeasuresWithMeasureIdAttachments:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures/{{measureId}}/attachments")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["get"](url, opts, ok, fail);
    },
 getMeasuresWithMeasureIdComments:function (opts, ok, fail) {
@@ -88,9 +86,7 @@ getMeasuresWithMeasureIdComments:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures/{{measureId}}/comments")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["get"](url, opts, ok, fail);
    },
 getMeasuresWithMeasuresId:function (opts, ok, fail) {
@@ -98,9 +94,7 @@ getMeasuresWithMeasuresId:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures/{{measuresId}}")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["get"](url, opts, ok, fail);
    },
 patchAttachmentsWithId:function (opts, ok, fail) {
@@ -108,9 +102,7 @@ patchAttachmentsWithId:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("attachments/{{id}}")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["patch"](url, opts, ok, fail);
    },
 patchMeasuresWithMeasureId:function (opts, ok, fail) {
@@ -118,9 +110,7 @@ patchMeasuresWithMeasureId:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures/{{measureId}}")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["patch"](url, opts, ok, fail);
    },
 postMeasures:function (opts, ok, fail) {
@@ -128,9 +118,7 @@ postMeasures:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["post"](url, opts, ok, fail);
    },
 postMeasuresWithMeasureIdAttachments:function (opts, ok, fail) {
@@ -138,9 +126,7 @@ postMeasuresWithMeasureIdAttachments:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures/{{measureId}}/attachments")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["post"](url, opts, ok, fail);
    },
 postMeasuresWithMeasureIdComments:function (opts, ok, fail) {
@@ -148,9 +134,7 @@ postMeasuresWithMeasureIdComments:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures/{{measureId}}/comments")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["post"](url, opts, ok, fail);
    },
 putAttachmentsWithId:function (opts, ok, fail) {
@@ -158,9 +142,7 @@ putAttachmentsWithId:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("attachments/{{id}}")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["put"](url, opts, ok, fail);
    },
 putMeasuresWithMeasureId:function (opts, ok, fail) {
@@ -168,9 +150,7 @@ putMeasuresWithMeasureId:function (opts, ok, fail) {
       this.init(opts);
        }
        let url = this.interpolate("measures/{{measureId}}")(opts);
-       if(this.debug){
-      console.log('url', url);
-       }
+       this.log.info(`hitting ${url}`);
        return this.restClient["put"](url, opts, ok, fail);
    },
 
