8b7e4a68d3a3bb66470c767c519365b38ceff1a9
Daniel Irwin <daniel.irwin@enernoc.com>
2018-01-30 18:04:23 -0500

    another bad reference

--- interceptors/lambdas.js
+++ interceptors/lambdas.js
@@ -67,7 +67,7 @@ module.exports = function (opts) {
     let injectableDataServices = utils.clientBuild(app.DataServices, activeEnvironment);
 
     let injectables = {
-        arupexlib : arupex.lib,
+        arupexlib : lib,
         logger: lib.logger,
         env: activeEnvironment,
         environment: activeEnvironment,
